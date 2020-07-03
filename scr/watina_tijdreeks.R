9d2fe5486ff23fa088964317e67aeb374893ffde
DriesAdriaens <dries.adriaens@inbo.be>
2020-07-03 14:11:33 +0200

    Further elaborate

--- scr/watina_tijdreeks.R
+++ scr/watina_tijdreeks.R
@@ -11,14 +11,15 @@ watina <- connect_watina()
 
 tijdreeks <-
   tbl(watina, "vwDimMeetpunt") %>% 
-  filter(MeetpuntCode %in% c("SILP003", "SILP002", "SILP001")) %>%
+  filter(MeetpuntCode %in% c("KASP001", "KASP002", "KASP032")) %>%
+  # filter(MeetpuntCode %in% c("SILP001", "SILP002", "SILP003", "SILP004")) %>%
   inner_join(tbl(watina, "FactPeilMeting"), by = "MeetpuntWID") %>% 
   inner_join(tbl(watina, "vwDimTijd"), by = c("TijdWID" = "DatumWID")) %>% 
   collect
 
 ggplot(tijdreeks) +
   geom_line(aes(x = date(Datum), y = mMaaiveld, colour = MeetpuntCode)) +
-  scale_y_continuous(limits = c(-0.75, 0.1)) +
+  coord_cartesian(ylim = c(-3, 0)) +
   scale_x_date(date_breaks = "year", date_labels = "%Y") +
   geom_hline(yintercept = 0, colour = "black", size = 1, linetype = "dotted") +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5),
@@ -33,5 +34,7 @@ tijdreeks %>% distinct(MeetpuntCode)
 tbl(watina, "vwDimMeetpunt") %>% 
   collect %>% 
   filter(grepl("SIL", MeetpuntCode))
+
+tbl(watina, "vwFactPeilMeting") %>% distinct(PeilmetingStatusCode)
 
 DBI::dbDisconnect(watina)
