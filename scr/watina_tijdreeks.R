4025585f9ae6169aeacde04ee24eea27124a7c6a
DriesAdriaens <dries.adriaens@inbo.be>
2020-07-03 16:47:02 +0200

    Finetuning

--- scr/watina_tijdreeks.R
+++ scr/watina_tijdreeks.R
@@ -7,52 +7,41 @@ library(stringr)
 
 watina <- connect_watina()
 
-#tabel met peilmetingen opvragen
+# 1. tabel met peilmetingen opvragen voor gewenste meetpunten ----
 
+locaties <- get_locs(watina,
+                     # area_codes = "SIL",
+                     # loc_type = "P",
+                     # loc_vec = c("KASP001", "KASP002", "KASP032"),
+                     loc_vec = c("SILP001", "SILP002", "SILP003", "SILP004"),
+                     loc_validity = c("VLD", "ENT"),
+                     collect = FALSE)
 tijdreeks <-
-  tbl(watina, "vwDimMeetpunt") %>% 
-  # filter(MeetpuntCode %in% c("KASP001", "KASP002", "KASP032")) %>%
-  filter(MeetpuntCode %in% c("SILP001", "SILP002", "SILP003", "SILP004")) %>%
-  inner_join(tbl(watina, "vwDimPeilpunt") %>%
-               select(-contains("Openbaarheid"),
-                      -contains("PeilpuntWID"),
-                      -contains("Naam")) %>%
-               distinct() %>%
-               mutate(mvTAW = ReferentieNiveauTAW - ReferentieNiveauMaaiveld) %>%
-               group_by(MeetpuntWID) %>%
-               summarise(mvTAW = mean(mvTAW)),
-             by = "MeetpuntWID") %>%
-  inner_join(tbl(watina, "FactPeilMeting"), by = "MeetpuntWID") %>% 
+  locaties %>% 
+  rename(mvTAW = soilsurf_ost) %>% 
+  inner_join(tbl(watina, "FactPeilMeting"), by = c("loc_wid" = "MeetpuntWID")) %>% 
   inner_join(tbl(watina, "vwDimTijd"), by = c("TijdWID" = "DatumWID")) %>% 
   collect
 
+# 2. figuur van tijdreeks(en) voor gewenste meetpunten ----
+
 ggplot(tijdreeks) +
-  geom_line(aes(x = date(Datum), y = mMaaiveld, colour = paste(MeetpuntCode,
-                                                               " (",
-                                                               round(mvTAW,2),
-                                                               " mTAW)"))) +
-# non clipping zoom
+  geom_line(aes(x = date(Datum), y = mMaaiveld,
+                colour = paste(loc_code, " (", round(mvTAW,2), " mTAW)"))) +
+  # non-clipping zoom
   # coord_cartesian(ylim = c(-1, 0.1)
   #                 , xlim = c(ymd("2009-01-01"), ymd("2017-01-01"))
   #                 ) +
+  # end non-clipping zoom
   scale_x_date(date_breaks = "year", date_labels = "%Y") +
-# clipping zoom
+  # clipping zoom
   # xlim(ymd("2009-01-01"), ymd("2017-01-01")) +
   # ylim(-1, 0.1) +
+  # end clipping zoom
   geom_hline(yintercept = 0, colour = "black", size = 1, linetype = "dotted") +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5),
         panel.grid.minor.x = element_line(linetype = "dashed")) +
   labs(x = "Datum", y = "m-mv", colour = "Meetpunt")
 
-tijdreeks %>% collect %>% View()
-
-unique(tijdreeks$MeetpuntCode)
-tijdreeks %>% distinct(MeetpuntCode)
-
-tbl(watina, "vwDimMeetpunt") %>% 
-  collect %>% 
-  filter(grepl("SIL", MeetpuntCode))
-
-tbl(watina, "vwFactPeilMeting") %>% distinct(PeilmetingStatusCode)
 
 DBI::dbDisconnect(watina)
