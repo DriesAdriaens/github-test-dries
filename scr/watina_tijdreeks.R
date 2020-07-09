a6c50c95b336b5ecef25332294609d35a10c50bb
DriesAdriaens <dries.adriaens@inbo.be>
2020-07-09 18:28:45 +0200

    further elaborate

--- scr/watina_tijdreeks.R
+++ scr/watina_tijdreeks.R
@@ -10,10 +10,11 @@ watina <- connect_watina()
 # 1. tabel met peilmetingen opvragen voor gewenste meetpunten ----
 
 locaties <- get_locs(watina,
-                     # area_codes = "SIL",
+                     # area_codes = "KRG",
                      # loc_type = c("P", "S", "R", "N", "W", "D", "L", "B"),
                      # loc_vec = c("KASP001", "KASP002", "KASP032"),
-                     loc_vec = c("SILP001", "SILP002", "SILP003", "SILP004"),
+                     # loc_vec = c("SILP001", "SILP002", "SILP003", "SILP004"),
+                     loc_vec = c("KRGP004"),
                      loc_validity = c("VLD", "ENT"),
                      # mask = NULL,
                      # join_mask = FALSE,
@@ -28,14 +29,16 @@ tijdreeks <-
   locaties %>% 
   rename(mvTAW = soilsurf_ost) %>% 
   inner_join(tbl(watina, "FactPeilMeting"), by = c("loc_wid" = "MeetpuntWID")) %>% 
-  inner_join(tbl(watina, "vwDimTijd"), by = c("TijdWID" = "DatumWID")) %>% 
-  collect
+  inner_join(tbl(watina, "DimTijd"), by = c("TijdWID" = "DatumWID")) %>% 
+  dplyr::collect()
 
 # 2. figuur van tijdreeks(en) voor gewenste meetpunten ----
 
 ggplot(tijdreeks) +
   geom_line(aes(x = date(Datum), y = mMaaiveld,
-                colour = paste(loc_code, " (", round(mvTAW,2), " mTAW)"))) +
+                colour = paste(loc_code, " (", round(mvTAW,2), " mTAW)")
+                # ,linetype = PeilmetingStatus
+                )) +
   # non-clipping zoom
   # coord_cartesian(ylim = c(-1, 0.1)
   #                 , xlim = c(ymd("2009-01-01"), ymd("2017-01-01"))
@@ -49,7 +52,23 @@ ggplot(tijdreeks) +
   geom_hline(yintercept = 0, colour = "black", size = 1, linetype = "dotted") +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5),
         panel.grid.minor.x = element_line(linetype = "dashed")) +
-  labs(x = "Datum", y = "m-mv", colour = "Meetpunt")
+  labs(x = "Datum", y = "m-mv", colour = "Meetpunt") +
+  # plotten van peilmetingen met representatieve periode > 30 dagen
+  geom_point(data = tijdreeks %>%
+               filter(ReprPeriode > 30),
+             aes(x = date(Datum), y = mMaaiveld))
+  # accentueren van specifieke tijdreeks
+  # geom_line(data = tijdreeks %>%
+  #               filter(loc_code == "ASHP013"),
+  #             aes(x = date(Datum), y = mMaaiveld),
+  #             colour = "black", size = 1.5)
 
 
 DBI::dbDisconnect(watina)
+
+tijdreeks %>% 
+  group_by(loc_code, HydroJaar) %>%
+  summarise(max_reprper = max(ReprPeriode)) %>% 
+  mutate(reprper_gr = ifelse(max_reprper > 30, 1, 0)) %>% 
+  group_by(loc_code, reprper_gr) %>% 
+  count()
