642870d6f4fdda8a63ad7851db2322528ceab959
DriesAdriaens <dries.adriaens@inbo.be>
2021-05-21 17:44:58 +0200

    Adapted

--- scr/watina_tijdreeks.R
+++ scr/watina_tijdreeks.R
@@ -15,8 +15,13 @@ locaties <- get_locs(watina,
                      # loc_type = c("P", "S", "R", "N", "W", "D", "L", "B"),
                      # loc_vec = c("KASP001", "KASP002", "KASP032"),
                      # loc_vec = c("SILP001", "SILP002", "SILP003", "SILP004"),
-                     loc_vec = c("GGVP011", "GGVP013", "GGVP015", "GGVP017", "GGVP033", "GGVP032"),
-                     loc_validity = c("VLD", "ENT", "DEL", "CLD"),
+                     loc_vec = c("VRIP008", "VRIP009", "VRIP010", "VRIP011", "VRIP012", "VRIP013"),
+                     # loc_vec = c("VRIP003", "VRIP006"
+                     #             #, "VRIP007"
+                     #             ),
+                     # loc_vec = c("VRIP004", "VRIP005"),
+                     # loc_validity = c("VLD", "ENT", "DEL", "CLD"),
+                     loc_validity = c("VLD"),
                      # mask = NULL,
                      # join_mask = FALSE,
                      # buffer = 10,
@@ -30,8 +35,27 @@ tijdreeks <-
   locaties %>% 
   rename(mvTAW = soilsurf_ost) %>% 
   inner_join(tbl(watina, "FactPeilMeting"), by = c("loc_wid" = "MeetpuntWID")) %>% 
-  inner_join(tbl(watina, "DimTijd"), by = c("TijdWID" = "DatumWID")) %>% 
-  dplyr::collect()
+  inner_join(tbl(watina, "DimTijd"), by = c("TijdWID" = "DatumWID")) %>%
+  dplyr::collect() %>% 
+  group_by(loc_code) %>%
+  arrange(loc_code, Datum) %>% 
+  mutate(
+    # reprp01 = ifelse(ReprPeriode > 60, 1, 0),
+    # reprp01_ = ifelse(reprp01 == 0 & lag(reprp01) == 1, 1, reprp01),
+    # reprp01_ = ifelse(is.na(reprp01_), reprp01, reprp01_),
+    # cumsum = cumsum(reprp01_),
+    # rnk_pct = percent_rank(cumsum),
+    # grp = loc_wid + rnk_pct/10,
+    # diff = Datum - lag(Datum),
+    diff = ifelse(is.na(Datum - lag(Datum)), ReprPeriode, Datum - lag(Datum)),
+    cumsum_dif = cumsum(diff > 60),
+    grp_diff = loc_wid + percent_rank(cumsum_dif)/10,
+    )
+
+# tijdreeks %>% 
+#   select(loc_code, Datum, mMaaiveld, mTAW) %>% 
+#   View()
+  
 
 # 2. figuur van tijdreeks(en) voor gewenste meetpunten ----
 
@@ -40,7 +64,8 @@ plot_tijdreeks <- ggplot(tijdreeks) +
   geom_line(aes(x = date(Datum),
                 y = mMaaiveld,
                 # y = mTAW,
-                colour = paste0(loc_code, " (", round(mvTAW,2), " mTAW)")
+                colour = paste0(loc_code, " (", round(mvTAW,2), " mTAW)"),
+                group = grp_diff
                 # Peilmetingstatus weergeven in legende (lijntype; ingegeven/gevalideerd)
                 # ,linetype = PeilmetingStatus
                 )) +
@@ -50,22 +75,30 @@ plot_tijdreeks <- ggplot(tijdreeks) +
   #                 ) +
   scale_x_date(date_breaks = "year", date_labels = "%Y") +
   ## clipping zoom
-  xlim(ymd("2018-01-01"), ymd("2020-08-01")) +
-  # ylim(-1, 0.1) +
-  geom_hline(yintercept = 0, colour = "black", size = 1, linetype = "dotted") +
+  # xlim(ymd("2018-01-01"), ymd("2020-08-01")) +
+  ylim(-0.85, 0.05) +
+  geom_hline(yintercept = 0, colour = "black", size = 1,
+             linetype = "dotted"
+             ) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5),
-        panel.grid.minor.x = element_line(linetype = "dashed")) +
+        panel.grid.minor.x = element_line(linetype = "dashed"),
+        legend.position = "bottom") +
   labs(x = "Datum", y = "m-mv", colour = NULL) +
   ## plotten van peilmetingen met representatieve periode > 30 dagen
-  geom_point(data = tijdreeks %>%
-               filter(ReprPeriode > 30),
-             aes(x = date(Datum), y = mMaaiveld))
+  geom_point(data = tijdreeks,
+             aes(x = date(Datum), y = mMaaiveld,
+                 colour = paste0(loc_code, " (", round(mvTAW,2), " mTAW)")),
+             size = 1)
+  # geom_point(data = tijdreeks %>%
+  #              filter(ReprPeriode > 30),
+  #            aes(x = date(Datum), y = mMaaiveld))
   ## accentueren van specifieke tijdreeks
   # geom_line(data = tijdreeks %>%
   #               filter(loc_code == "ASHP013"),
   #             aes(x = date(Datum), y = mMaaiveld),
   #             colour = "black", size = 1.5)
-  
+
+ggsave("plot2.png", plot_tijdreeks, dpi = 300, width = 15, units = "cm")  
 plot_tijdreeks
 
 xg3 <- get_xg3(locs = locaties, con = watina,
@@ -93,8 +126,10 @@ reftab <- read.csv("C:/Users/dries_adriaens/Documents/niche_vegetation.csv")
 
 ggplot(reftab %>% 
          select(-nutrient_level, - acidity, -management, -inundation) %>% 
+         # filter(veg_code %in% c(14, 17, 18)) %>% 
          distinct()) +
-  geom_linerange(aes(x = factor(veg_code), ymin = -mhw_min, ymax = -mhw_max,
+  geom_linerange(aes(x = factor(veg_code),
+                     ymin = -mhw_min, ymax = -mhw_max,
                      colour = soil_name),
                  position = position_dodge(0.5),
                  size = 1) +
@@ -106,8 +141,8 @@ ggplot(reftab %>%
   #                             values_to = "mw_value"),
   #              aes(x = factor(veg_code), y = -mw_value),
   #              alpha = 0.5) +
-  labs(x = "Vegetatietype", y = "bereik ghg (cm-mv)", colour = "bodemtype") +
-  ylim(c(-300, 50))
+  geom_hline(yintercept = 0) +
+  labs(x = "Vegetatietype", y = "bereik ghg (cm-mv)", colour = "bodemtype")
 
 ggplot(reftab %>% 
          select(-nutrient_level, - acidity, -management, -inundation) %>% 
@@ -126,6 +161,66 @@ ggplot(reftab %>%
   #              alpha = 0.5) +
   labs(x = "Vegetatietype", y = "bereik glg (cm-mv)", colour = "bodemtype") +
   ylim(c(-300, 50))
+
+ggplot(reftab %>% 
+         select(-nutrient_level, - acidity, -management, -inundation) %>% 
+         filter(veg_code %in% c(14, 17, 18)) %>% 
+         distinct()) +
+  geom_linerange(aes(#x = factor(veg_code),
+    x = veg_type,
+    ymin = -mhw_min, ymax = -mhw_max,
+    colour = soil_name),
+    position = position_dodge(0.5),
+    size = 1) +
+  # geom_boxplot(data = reftab %>%
+  #                select(-nutrient_level, - acidity, -management, -inundation) %>%
+  #                distinct() %>%
+  #                pivot_longer(starts_with("m"),
+  #                             names_to = "mw_type",
+  #                             values_to = "mw_value"),
+  #              aes(x = factor(veg_code), y = -mw_value),
+  #              alpha = 0.5) +
+  geom_hline(yintercept = 0) +
+  labs(x = "Vegetatietype", y = "bereik ghg (cm-mv)", colour = "bodemtype")# +
+#ylim(c(-300, 50))
+
+reftab_long <- reftab %>% 
+  select(-nutrient_level, - acidity, -management, -inundation) %>% 
+  distinct() %>% 
+  pivot_longer(starts_with("m"), 
+               names_to = "mw_var", 
+               values_to = "mw_value") %>% 
+  mutate(mw_type = ifelse(grepl("^mh", mw_var), "GHG", "GLG"),
+         mw_minmax = ifelse(grepl("min$", mw_var), "min", "max")) %>% 
+  select(-mw_var) %>% 
+  pivot_wider(names_from = mw_minmax, values_from = mw_value)
+
+ggplot(reftab_long %>%
+         filter(veg_code %in% 
+                  # c(14, 17, 18)
+                c(2, 4))) +
+  geom_linerange(aes(#x = factor(veg_code),
+    x = veg_type,
+    ymin = -min, ymax = -max,
+    colour = soil_name),
+    position = position_dodge(0.5),
+    size = 1) +
+  geom_hline(yintercept = 0) +
+  coord_flip() +
+  facet_wrap(~ mw_type) +
+  labs(x = "Vegetatietype", y = "waterpeil (cm-mv)", colour = "bodemtype")
+ggsave("NICHE_sel_91EO_vmva.jpeg", width = 15, height = 7, units = "cm")
+
+ggplot() +
+  geom_boxplot(data = reftab %>%
+                 select(-nutrient_level, - acidity, -management, -inundation) %>% 
+                 distinct() %>% 
+                 pivot_longer(starts_with("m"), 
+                              names_to = "mw_type", 
+                              values_to = "mw_value"), 
+               aes(x = reorder(veg_code, mw_value, median), y = -mw_value/100)) +
+  geom_hline(yintercept = 0, colour = "black", size = 1, linetype = "dotted") +
+  labs(x = "Vegetatietype", y = "Referentiebereik GXG (m-mv)")
 
 ggplot() +
   geom_boxplot(data = reftab %>%
@@ -218,8 +313,9 @@ locaties_TAW <- get_locs(watina,
                      loc_type = c("P", "S"),
                      # loc_vec = c("KASP001", "KASP002", "KASP032"),
                      # loc_vec = c("SILP001", "SILP002", "SILP003", "SILP004"),
-                     loc_vec = c("GGVP011", "GGVP013", "GGVP015", "GGVP017", "GGVP032", "GGVP033",
-                                 "GGVS003","GGVS004", "GGVS010", "GGVS011"),
+                     # loc_vec = c("GGVP011", "GGVP013", "GGVP015", "GGVP017", "GGVP032", "GGVP033",
+                     #             "GGVS003","GGVS004", "GGVS010", "GGVS011"),
+                     loc_vec = c("VRIP003", "VRIP006", "VRIP007"),
                      loc_validity = c("VLD", "ENT", "DEL", "CLD"),
                      # mask = NULL,
                      # join_mask = FALSE,
@@ -260,16 +356,16 @@ plot_tijdreeks_TAW <- ggplot(tijdreeks_TAW) +
   #                 ) +
   scale_x_date(date_breaks = "year", date_labels = "%Y") +
   ## clipping zoom
-  xlim(ymd("2018-01-01"), ymd("2020-08-01")) +
+  # xlim(ymd("2018-01-01"), ymd("2020-08-01")) +
   # ylim(-1, 0.1) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5),
         panel.grid.minor.x = element_line(linetype = "dashed")) +
-  labs(x = "Datum", y = "mTAW", colour = "Meetpunt") +
+  labs(x = "Datum", y = "mTAW", colour = "Meetpunt")
   ## plotten van peilmetingen met representatieve periode > 30 dagen
-  geom_point(data = tijdreeks_TAW %>%
-               filter(ReprPeriode > 30),
-             aes(x = date(Datum), y = mTAW)) +
-  theme(text = element_text(size = 16))
+  # geom_point(data = tijdreeks_TAW %>%
+  #              filter(ReprPeriode > 30),
+  #            aes(x = date(Datum), y = mTAW)) +
+  # theme(text = element_text(size = 16))
 ## accentueren van specifieke tijdreeks
 # geom_line(data = tijdreeks %>%
 #               filter(loc_code == "ASHP013"),
