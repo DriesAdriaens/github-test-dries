7c560fac2e6e2cb417c29f9e491d980dadb55958
DriesAdriaens <dries.adriaens@inbo.be>
2021-05-28 15:18:21 +0200

    Finetune

--- scr/VMM_oppwat.R
+++ scr/VMM_oppwat.R
@@ -4,15 +4,27 @@ library(dplyr)
 library(stringr)
 library(tidyr)
 
-Watkwal_agg <- readxl::read_xlsx("G:/Mijn Drive/Projecten/PRJ_SBO_Future_Floodplains/140_RawData/Systeembeschrijvingen/Dijle/FysicochemischeMetingenPerMeetpuntJaarParameterBerekening_Dijle.xlsx", sheet = "Metingen per jaar", range = "A8:J14495")
+setwd("G:/Mijn Drive/Projecten/PRJ_SBO_Future_Floodplains/140_RawData/Systeembeschrijvingen/Dijle/")
+
+Watkwal_agg <- readxl::read_xlsx("FysicochemischeMetingenPerMeetpuntJaarParameterBerekening_Dijle.xlsx", sheet = "Metingen per jaar", range = "A8:J14495")
 
 Watkwal_agg_sel <- Watkwal_agg %>% 
   filter((Symbool == "NO3-" & Berekening == "Percentiel90")|
-           (Symbool == "oPO4" & Berekening == "Gemiddelde"),
+           (Symbool == "oPO4" & Berekening == "Gemiddelde")|
+           (Symbool == "KjN" & Berekening == "Percentiel90"),
          Meetpunt != 220900) %>% 
-  mutate(Symbool = ifelse(Symbool == "NO3-", "N-NO3", "P-PO4"),
-         Meetpunt_naam = ifelse(Meetpunt == "220500", "Korbeek-Dijle", "St-Joris-Weert"),
-         Jaar_datum = ymd(paste0(Jaar, "0601"))) %>% 
+  mutate(
+    BKN = case_when(
+      Symbool == "NO3-" ~ 5.65,
+      Symbool == "oPO4" ~ 0.14,
+      Symbool == "KjN" ~ 6),
+    Symbool = case_when(
+      Symbool == "NO3-" ~ "N-NO3",
+      Symbool == "oPO4" ~ "P-PO4",
+      Symbool == "KjN" ~ "N-KjN",
+      TRUE ~ Symbool),
+    Meetpunt_naam = ifelse(Meetpunt == "220500", "220500; Korbeek-Dijle", "221000; St-Joris-Weert"),
+    Jaar_datum = ymd(paste0(Jaar, "0601"))) %>% 
   group_by(Meetpunt, Symbool) %>%
   arrange(Meetpunt, Symbool, Jaar) %>% 
   #berekenen van group variabele om lijngrafiek te onderbreken bij ontbrekende jaren
@@ -23,20 +35,24 @@ Watkwal_agg_sel <- Watkwal_agg %>%
 
 ggplot(Watkwal_agg_sel,
        aes(x = Jaar, y = Waarde, colour = Meetpunt_naam)) +
+  geom_line(aes(x = Jaar, y = BKN, linetype = Symbool), colour = "black", size = 1) +
   geom_point() +
   geom_line(aes(group = grp_diff)) +
   facet_wrap(~Symbool, scales = "free_y") +
-  labs(y = "mg/L", colour = "Meetpunt") +
-  theme_bw()
+  labs(y = "mg/L", colour = "Meetpunt", linetype = "Basismilieunorm") +
+  scale_x_continuous(minor_breaks = seq(1900,2050,2), limits = c(1989, 2021)) +
+  theme_bw() + 
+  theme(legend.position = "bottom")
+ggsave("Dijle_Waterkwaliteit_VMM_agg.png", dpi = 300, width = 15, height = 10, units = "cm")
 
 
-Watkwal <- readxl::read_xlsx("G:/Mijn Drive/Projecten/PRJ_SBO_Future_Floodplains/140_RawData/Systeembeschrijvingen/Dijle/FysicochemischeMetingenPerMeetpuntJaarParameterBerekening_Dijle.xlsx", sheet = "Metingen per datum", range = "A8:I20542")
+Watkwal <- readxl::read_xlsx("FysicochemischeMetingenPerMeetpuntJaarParameterBerekening_Dijle.xlsx", sheet = "Metingen per datum", range = "A8:I20542")
 
 Watkwal_sel <- Watkwal %>% 
   filter((Symbool == "NO3-" | Symbool == "oPO4"),
          Meetpunt != 220900) %>% 
   mutate(Symbool = ifelse(Symbool == "NO3-", "N-NO3", "P-PO4"),
-         Meetpunt_naam = ifelse(Meetpunt == "220500", "Korbeek-Dijle", "St-Joris-Weert"))
+         Meetpunt_naam = ifelse(Meetpunt == "220500", "220500; Korbeek-Dijle", "221000; St-Joris-Weert"))
 
 ggplot(Watkwal_sel,
        aes(x = Datum, y = Waarde, colour = Meetpunt_naam)) +
